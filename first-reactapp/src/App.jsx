8223cb420adc232f3ada1cc0e83081f463953368
32harshitha <harshithamallipeddu@gmail.com>
2026-06-05 11:22:15 +0530

    Add day-11 React project

---
+++ first-reactapp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React 🚀</h1>
+      <p>I created my first React application.</p>
+    </div>
+  )
+}
+
+export default App
